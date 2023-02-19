c10007636f9ea9e80147724ddd8157c388d09977
AzumattDev <azumattgaming@gmail.com>
2023-02-19 09:05:36 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,121 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ILikeToBeAnnoyed
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class ILikeToBeAnnoyedPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "ILikeToBeAnnoyed";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        internal static string ConnectionError = "";
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource ILikeToBeAnnoyedLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        #region ConfigOptions
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        #endregion
+    }
+    
+    [HarmonyPatch(typeof(MonsterAI), nameof(MonsterAI.Start))]
+    static class MonsterAIAwakePatch
+    {
+        static void Postfix(MonsterAI __instance)
+        {
+            if (!__instance.m_nview || !__instance.m_nview.IsValid() || !__instance.m_nview.IsOwner())
+                return;
+
+            if (ZNetScene.instance == null)
+                return;
+
+            if (Utils.GetPrefabName(__instance.gameObject).StartsWith("Wolf"))
+            {
+                __instance.m_idleSound = new EffectList()
+                {
+                    m_effectPrefabs = new EffectList.EffectData[]
+                    {
+                        new()
+                        {
+                            m_prefab = ZNetScene.instance.GetPrefab("sfx_wolf_haul"),
+                            m_enabled = true,
+                            m_variant = -1,
+                            m_attach = false,
+                            m_inheritParentRotation = false,
+                            m_inheritParentScale = false,
+                            m_randomRotation = false,
+                            m_scale = false,
+                            m_childTransform = ""
+                        },
+                    }
+                };
+            }
+        }
+    }
+    
+    
+    [HarmonyPatch(typeof(ZNetScene),nameof(ZNetScene.Awake))]
+    static class ZNetSceneAwakePatch
+    {
+        static void Prefix(ZNetScene __instance)
+        {
+            foreach (GameObject gameObject in Resources.FindObjectsOfTypeAll<GameObject>())
+            {
+                if (gameObject.name != "sfx_wolf_haul") continue; 
+                // The default game doesn't have this registered in the ZNetScene. Force it.
+                __instance.m_prefabs.Add(gameObject);
+                break;
+            }
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Description
+
+## Makes the tamed wolves howl once more. How annoying is that! This mod was a request by `Unforgiven_0_ a.k.a Ilias#7350`
+
+`This mod is not needed on a server. It's client only. Install on each client that you wish to have the mod load. If you are using this on a server, you may notice that the wolves will not howl sometimes. This is because the owner of the chunk is the person that will control the behaviour of spawned creatures. If they do not have the mod, this will not take effect.`
+
+`Feel free to reach out to me on discord if you need manual download assistance.`
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/Pb6bVMnFb2)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
+***
+
+> # Update Information (Latest listed first)
+
+| `Version` | `Update Notes`    |
+|-----------|-------------------|
+| 1.0.0     | - Initial Release |
