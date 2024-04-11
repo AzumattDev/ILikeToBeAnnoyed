ef6b0214fc5085a4fe5552a2263126e641addc84
AzumattDev <azumattgaming@gmail.com>
2024-04-11 04:13:34 -0400

    v1.0.1

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ namespace ILikeToBeAnnoyed
     public class ILikeToBeAnnoyedPlugin : BaseUnityPlugin
     {
         internal const string ModName = "ILikeToBeAnnoyed";
-        internal const string ModVersion = "1.0.0";
+        internal const string ModVersion = "1.0.1";
         internal const string Author = "Azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
---
+++ Thunderstore/CHANGELOG.md
@@ -0,0 +1,6 @@
+> # Update Information (Latest listed first)
+
+| `Version` | `Update Notes`                         |
+|-----------|----------------------------------------|
+| 1.0.1     | - Courtesy update for Valheim 0.217.46 |
+| 1.0.0     | - Initial Release                      |
